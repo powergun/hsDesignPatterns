bc5241cdaef33a26b5156dbfa09837b526adb73a
Wei Ning <macgnw@gmail.com>
2020-01-08 08:01:18 +1100

    notes

--- has-typeclass/README.md
+++ has-typeclass/README.md
@@ -3,4 +3,11 @@
 inspired by RIO tutorial: <https://tech.fpcomplete.com/haskell/library/rio>
 has-typeclass example
 
-## Use Has-Typeclass to enforce compliance
+## Use Has-Typeclass to enforce (data) compliance
+
+to enforce that some unknown type must be able provide some critical
+information, define a custom typeclass, calling it `HasXXX` - XXX
+being the critical information, then use the standard type constraint
+to express this requirement on the function type signature
+
+see src/MustHaveHandle.hs
--- has-typeclass/src/MustHaveHandle.hs
+++ has-typeclass/src/MustHaveHandle.hs
@@ -3,8 +3,10 @@ module MustHaveHandle (demo) where
 import           RIO
 import           System.IO (hPutStrLn, stderr, stdout)
 
--- the restriction is only that, a type must have a "handle" field,
+-- the constraint is only that, a type must have a "handle" field,
 -- and must know how to provide this "handle" to the caller
+-- I have two types here that can fulfill this duty, along with a third
+-- type, which is the native Handle type
 data SimpleConf = SimpleConf { simpleHandle :: Handle }
 data AppConf = AppConf { appHandle :: Handle, appName :: String }
 
@@ -17,12 +19,14 @@ instance HasHandle SimpleConf where
 instance HasHandle AppConf where
   getHandle = appHandle
 
--- support a wide range of types
+-- this function is called in several code paths, where different
+-- types are threaded via the reader's environment
 process :: HasHandle env => String -> RIO env ()
 process s =
   asks getHandle >>= (\h -> liftIO . (hPutStrLn h) . ("use 'idnoclip' in " ++) $ s)
 
--- support only specific type(s)
+-- this function is called in a specific code path, where a specific
+-- compliant data type is threaded via the reader's environment
 compute :: RIO AppConf ()
 compute =
   asks appName >>= process
