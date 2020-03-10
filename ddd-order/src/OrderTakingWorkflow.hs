13fc6a19e9687e9f2ff56c79c083268fc10f7929
Wei Ning <macgnw@gmail.com>
2020-03-10 15:42:35 +1100

    ddd fp book

--- ddd-order/src/OrderTakingWorkflow.hs
+++ ddd-order/src/OrderTakingWorkflow.hs
@@ -1,5 +1,9 @@
 module OrderTakingWorkflow where
 
+-- see: https://jeltsch.wordpress.com/2012/03/20/natural-numbers-in-haskell/
+-- source: http://hackage.haskell.org/package/natural-numbers
+import           Data.Natural
+
 -- from DDD FP book L641
 -- model the book's example in haskell
 
@@ -14,3 +18,31 @@ data AmountToBill
 
 data Order =
     Order CustomerInfo ShippingAddress BillingAddress [OrderLine] AmountToBill
+
+-- DDD FP book L728
+
+data WidgetCode = WidgetCode String  -- data ctor is private, same as below
+makeWidgetCode :: [Char] -> WidgetCode
+makeWidgetCode s@('W':x:y:z:w) = WidgetCode s
+makeWidgetCode _               = undefined
+
+data GizmoCode = GizmoCode String
+makeGizmoCode s@('G':x:y:z:w) = GizmoCode s
+makeGizmoCode _               = undefined
+
+data ProductCode = PW WidgetCode | PG GizmoCode
+
+-- another to enforce the value range restriction on the type level is
+-- using a smart contructor (like the makeXXX functions above)
+-- see also: https://stackoverflow.com/questions/8510570/restricting-values-in-type-constructors
+-- EDIT: I need to use smart ctor anyway to follow the book examples
+data OrderQuantity = OUn UnitQuantity | OKi KilogramQuantity
+data UnitQuantity = UnitQuantity Natural
+makeUnitQuantity x = if (x >= 1 && x <= 1000)
+                     then UnitQuantity x
+                     else undefined
+
+data KilogramQuantity = KilogramQuantity Float
+makeKilogramQuantity x = if (x >= 0.05 && x <=100.0)
+                         then KilogramQuantity x
+                         else undefined
