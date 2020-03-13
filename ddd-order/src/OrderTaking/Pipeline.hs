4d45042bf1c4e68145d4063ce1414e9b37c2a72f
Wei Ning <wei.ning@digitalasset.com>
2020-03-13 15:29:16 +1100

    ddd fp book wip

--- ddd-order/src/OrderTaking/Pipeline.hs
+++ ddd-order/src/OrderTaking/Pipeline.hs
@@ -3,14 +3,14 @@ module OrderTaking.Pipeline where
 import Data.Time.Clock (UTCTime)
 
 data UnvalidatedOrder = UnvalidatedOrder
-    { orderId :: String 
-    , customerInfo :: UnvalidatedCustomer
-    , shippingaddress :: UnvalidatedAddress 
+    { unOrderId :: String 
+    , unCustomerInfo :: UnvalidatedCustomer
+    , unShippingaddress :: UnvalidatedAddress 
     }
 
 data UnvalidatedCustomer = UnvalidatedCustomer 
-    { name :: String
-    , email :: String
+    { unName :: String
+    , unEmail :: String
     }
 
 data UnvalidatedAddress = UnvalidatedAddress String
@@ -37,3 +37,45 @@ type AsyncResult a b = Either a b
 placeOrderWorkflow :: PlaceOrderCommand -> AsyncResult PlaceOrderError [PlaceOrderEvent]
 placeOrderWorkflow = undefined
 
+-- Order Life Cycle
+data ValidatedOrderLine
+data ValidatedOrder = ValidatedOrder
+    { orderId :: String 
+    , customerInfo :: CustomerInfo
+    , shippingAddress :: String
+    , billingAddress :: String
+    , orderLines :: [ValidatedOrderLine]
+    }
+data CustomerInfo
+data PricedOrderLine
+data PricedOrder
+
+data Order = Unvalidated UnvalidatedOrder
+           | Validated ValidatedOrder
+           | Priced PricedOrder
+
+-- Internal Steps
+data ProductCode
+checkProductCodeExists :: ProductCode -> Bool
+checkProductCodeExists = undefined
+
+data AddressValidationError
+data CheckedAddress
+checkAddressExists :: UnvalidatedAddress
+                   -> AsyncResult AddressValidationError CheckedAddress
+checkAddressExists = undefined
+
+data ValidationError
+validateOrder :: (ProductCode -> Bool)
+              -> (UnvalidatedAddress -> AsyncResult AddressValidationError CheckedAddress)
+              -> AsyncResult [ValidationError] ValidatedOrder
+validateOrder = undefined
+
+newtype Price = Price Int
+data PricingError
+getProductPrice :: ProductCode -> Price
+getProductPrice = undefined
+priceOrder :: (ProductCode -> Price)
+           -> ValidatedOrder
+           -> Either PricingError PricedOrder
+priceOrder = undefined
